9ef44e36903d58663433f2fb5d0e54838b18f325
tyange <usun16@gmail.com>
2022-11-05 19:52:21 +0900

    burger controller에 entity import

--- src/burger/burger.module.ts
+++ src/burger/burger.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Burger } from './burger.entity';
 import { BurgerController } from './burger.controller';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Burger])],
   providers: [],
   controllers: [BurgerController],
 })
