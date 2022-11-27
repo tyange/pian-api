fd013443c3720d6b82bce6cee2b26c2d7d5c12e7
tyange <usun16@gmail.com>
2022-11-27 12:40:03 +0900

    burger 삭제 기능

--- src/burger/burger.controller.ts
+++ src/burger/burger.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Burger } from './burger.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { AddBurgerDto } from './dto/add-burger.dto';
+import { EditBurgerDto } from './dto/edit-burger.dto';
 
 @Controller('burger')
 export class BurgerController {
@@ -10,6 +19,20 @@ export class BurgerController {
     @InjectRepository(Burger)
     private burgerRepository: Repository<Burger>,
   ) {}
+
+  @Get()
+  getAllBurger() {
+    return this.burgerRepository.find();
+  }
+
+  @Get(':id')
+  getBurger(@Param('id') burgerId: number) {
+    return this.burgerRepository.findOne({
+      where: {
+        id: burgerId,
+      },
+    });
+  }
 
   @Post()
   addBurger(@Body() addBurgerDto: AddBurgerDto): Promise<Burger> {
@@ -20,5 +43,30 @@ export class BurgerController {
     });
 
     return this.burgerRepository.save(burger);
+  }
+
+  @Put(':id')
+  async editBurger(
+    @Param('id') editBurgerId: number,
+    @Body() editBurgerDto: EditBurgerDto,
+  ): Promise<Burger> {
+    const targetBurger = await this.burgerRepository.findOne({
+      where: {
+        id: editBurgerId,
+      },
+    });
+
+    targetBurger.name = editBurgerDto.name;
+    targetBurger.brand = editBurgerDto.brand;
+    targetBurger.description = editBurgerDto.description;
+
+    return this.burgerRepository.save(targetBurger);
+  }
+
+  @Delete(':id')
+  async deleteBurger(
+    @Param('id') deleteBurgerId: number,
+  ): Promise<DeleteResult> {
+    return this.burgerRepository.delete(deleteBurgerId);
   }
 }
---
+++ src/burger/dto/edit-burger.dto.ts
@@ -0,0 +1,5 @@
+export class EditBurgerDto {
+  name: string;
+  brand: string;
+  description: string;
+}
